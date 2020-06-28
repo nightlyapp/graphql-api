b318c9616693625a4a59b52bbf6cb44cb9d8d5c6
Pedro de Almeida <pedro.de.almeida@avanade.com>
2020-06-28 17:33:20 -0300

    criando pasta dist

--- index.dev.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// for development purposes only, we include module-alias
-Object.defineProperty(exports, "__esModule", { value: true });
-require("module-alias/register");
-require("./src/index");
--- src/db/models.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const pub_1 = require("./models/pub");
-const geolocation_1 = require("./models/geolocation");
-exports.default = [pub_1.Pub, geolocation_1.Geolocation];
